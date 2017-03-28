ab1e2077b323c2d4456b29a86aded28f4f685b01
tonkotsuboy <t.kano.624@gmail.com>
2017-03-28 10:21:01 +0900

    update output files

--- src/js/script.js
+++ src/js/script.js
@@ -11,16 +11,16 @@
 /******/
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
 /******/ 		};
 /******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 /******/
 /******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
+/******/ 		module.l = true;
 /******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
@@ -33,140 +33,169 @@
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
 /******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	"use strict";
-	var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-	    return new (P || (P = Promise))(function (resolve, reject) {
-	        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-	        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-	        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-	        step((generator = generator.apply(thisArg, _arguments || [])).next());
-	    });
-	};
-	var __generator = (this && this.__generator) || function (thisArg, body) {
-	    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
-	    return { next: verb(0), "throw": verb(1), "return": verb(2) };
-	    function verb(n) { return function (v) { return step([n, v]); }; }
-	    function step(op) {
-	        if (f) throw new TypeError("Generator is already executing.");
-	        while (_) try {
-	            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
-	            if (y = 0, t) op = [0, t.value];
-	            switch (op[0]) {
-	                case 0: case 1: t = op; break;
-	                case 4: _.label++; return { value: op[1], done: false };
-	                case 5: _.label++; y = op[1]; op = [0]; continue;
-	                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-	                default:
-	                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-	                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-	                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-	                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-	                    if (t[2]) _.ops.pop();
-	                    _.trys.pop(); continue;
-	            }
-	            op = body.call(thisArg, _);
-	        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-	        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-	    }
-	};
-	Object.defineProperty(exports, "__esModule", { value: true });
-	var SubModule_1 = __webpack_require__(1);
-	var EventName_1 = __webpack_require__(2);
-	var Main = (function () {
-	    function Main() {
-	        this.hello();
-	        var sub = new SubModule_1.default();
-	        sub.hello();
-	        this.checkAsync();
-	    }
-	    Main.prototype.hello = function () {
-	        console.log("log from Main Class");
-	    };
-	    /**
-	     * await/asyncのテストコマンド
-	     * @returns {Promise<void>}
-	     */
-	    Main.prototype.checkAsync = function () {
-	        return __awaiter(this, void 0, void 0, function () {
-	            var i;
-	            return __generator(this, function (_a) {
-	                switch (_a.label) {
-	                    case 0:
-	                        console.log("start");
-	                        i = 0;
-	                        _a.label = 1;
-	                    case 1:
-	                        if (!(i < 50)) return [3 /*break*/, 4];
-	                        return [4 /*yield*/, this.delay(30)];
-	                    case 2:
-	                        _a.sent();
-	                        console.log(i + ":works");
-	                        _a.label = 3;
-	                    case 3:
-	                        i++;
-	                        return [3 /*break*/, 1];
-	                    case 4: return [4 /*yield*/, this.delay(30)];
-	                    case 5:
-	                        _a.sent();
-	                        console.log("end!");
-	                        return [2 /*return*/];
-	                }
-	            });
-	        });
-	    };
-	    Main.prototype.delay = function (milliseconds) {
-	        return new Promise(function (resolve) {
-	            setTimeout(resolve, milliseconds);
-	        });
-	    };
-	    return Main;
-	}());
-	window.addEventListener(EventName_1.default.DOM_CONTENT_LOADED, function () { return new Main(); });
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var EventName = (function () {
+    function EventName() {
+    }
+    return EventName;
+}());
+EventName.DOM_CONTENT_LOADED = "DOMContentLoaded";
+exports.default = EventName;
 
 
-/***/ },
+/***/ }),
 /* 1 */
-/***/ function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	"use strict";
-	Object.defineProperty(exports, "__esModule", { value: true });
-	var SubModule = (function () {
-	    function SubModule() {
-	    }
-	    SubModule.prototype.hello = function () {
-	        console.log("log from Sub");
-	    };
-	    return SubModule;
-	}());
-	exports.default = SubModule;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var SubModule = (function () {
+    function SubModule() {
+    }
+    SubModule.prototype.hello = function () {
+        console.log("log from Sub");
+    };
+    return SubModule;
+}());
+exports.default = SubModule;
 
 
-/***/ },
+/***/ }),
 /* 2 */
-/***/ function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	"use strict";
-	Object.defineProperty(exports, "__esModule", { value: true });
-	var EventName = (function () {
-	    function EventName() {
-	    }
-	    return EventName;
-	}());
-	EventName.DOM_CONTENT_LOADED = "DOMContentLoaded";
-	exports.default = EventName;
+"use strict";
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
+    return { next: verb(0), "throw": verb(1), "return": verb(2) };
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var SubModule_1 = __webpack_require__(1);
+var EventName_1 = __webpack_require__(0);
+var Main = (function () {
+    function Main() {
+        this.hello();
+        var sub = new SubModule_1.default();
+        sub.hello();
+        this.checkAsync();
+    }
+    Main.prototype.hello = function () {
+        console.log("log from Main Class");
+    };
+    /**
+     * await/asyncのテストコマンド
+     * @returns {Promise<void>}
+     */
+    Main.prototype.checkAsync = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var i;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        console.log("start");
+                        i = 0;
+                        _a.label = 1;
+                    case 1:
+                        if (!(i < 50)) return [3 /*break*/, 4];
+                        return [4 /*yield*/, this.delay(30)];
+                    case 2:
+                        _a.sent();
+                        console.log(i + ":works");
+                        _a.label = 3;
+                    case 3:
+                        i++;
+                        return [3 /*break*/, 1];
+                    case 4: return [4 /*yield*/, this.delay(30)];
+                    case 5:
+                        _a.sent();
+                        console.log("end!");
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    Main.prototype.delay = function (milliseconds) {
+        return new Promise(function (resolve) {
+            setTimeout(resolve, milliseconds);
+        });
+    };
+    return Main;
+}());
+window.addEventListener(EventName_1.default.DOM_CONTENT_LOADED, function () { return new Main(); });
 
 
-/***/ }
+/***/ })
 /******/ ]);
 //# sourceMappingURL=script.js.map
