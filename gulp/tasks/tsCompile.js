dfb46e88ab8088e82a75b8d9610bb6bfe1010fa5
tonkotsuboy <t.kano.624@gmail.com>
2017-03-28 14:12:04 +0900

    omit gulp-sorucemaps for ts

--- gulp/tasks/tsCompile.js
+++ gulp/tasks/tsCompile.js
@@ -8,18 +8,11 @@ const config = require("../../build_config")
 const taskName = require("../taskName")
 
 const gulp = require("gulp")
-const webpackStream = require("webpack-stream");
-const webpack = require("webpack");
-
-const webpackConfig = require('../../webpack.config.js')
-
-
-const sourcemaps = require("gulp-sourcemaps")
-
-// TODO:webpak 2 fix
+const webpackStream = require("webpack-stream")
+const webpack = require("webpack")
+const webpackConfig = require("../../webpack.config.js")
 
 gulp.task(taskName.tsCompile, () => {
   return webpackStream(webpackConfig, webpack)
-    // .pipe(sourcemaps.write("./"))
     .pipe(gulp.dest(config.jsFolder))
 })
