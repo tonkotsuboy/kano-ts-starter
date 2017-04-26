82527356741a263d4ceee46dca200b06c4cf03d8
tonkotsuboy <t.kano.624@gmail.com>
2017-04-26 15:03:02 +0900

    add ts error handling

--- gulp/tasks/tsCompile.js
+++ gulp/tasks/tsCompile.js
@@ -6,6 +6,9 @@
 const config = require("../../build_config")
 
 const taskName = require("../taskName")
+const notify = require('gulp-notify');
+
+const plumber = require("gulp-plumber")
 
 const gulp = require("gulp")
 const webpackStream = require("webpack-stream")
@@ -13,6 +16,7 @@ const webpack = require("webpack")
 const webpackConfig = require("../../webpack.config.js")
 
 gulp.task(taskName.tsCompile, () => {
-  return webpackStream(webpackConfig, webpack)
+  return plumber({errorHandler: notify.onError('<%= error.message %>')})
+    .pipe(webpackStream(webpackConfig, webpack))
     .pipe(gulp.dest(config.jsFolder))
 })
