1d115f360dd130f52f4b248222a0e9ff1f442758
tonkotsuboy <t.kano.624@gmail.com>
2018-04-27 16:32:55 +0900

    update package and core-jsa

--- src/ts/Main.ts
+++ src/ts/Main.ts
@@ -1,17 +1,16 @@
-import "es6-promise/auto";
 import {EventName} from "./eventname/EventName";
 import SubModule from "./sub/SubModule";
+import AsyncTest from "./test/AsyncTest";
 
 // import AsyncTest from "./test/AsyncTest";
 
 class Main {
   constructor() {
-    document.write("MainClass works!  ");
     const sub = new SubModule();
     sub.hello();
 
     // Promiseのawait/asyncを試したい場合は、import共にコメントアウトを外す。
-    // new AsyncTest();
+    new AsyncTest();
   }
 }
 
